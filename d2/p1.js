4510d2b178738fefe287c112dcecbde2611ae7e1
Babacar <bndoyecs@gmail.com>
2024-12-12 21:58:47 -0500

    rewriting code for future problems

--- d2/p1.js
+++ d2/p1.js
@@ -3,29 +3,41 @@ const { input } = require("./input");
 let safeCount = 0;
 input.forEach((line) => {
   const allNumbers = line.split(" ");
-  let prevNum = parseInt(allNumbers[0]);
-  let isDecreasing = false;
 
-  if (prevNum > allNumbers[1]) {
-    isDecreasing = true;
+  // console.log(allNumbers, isDecreasing(allNumbers), isIncreasing(allNumbers));
+  if (isDecreasing(allNumbers)) {
+    safeCount++;
+  } else if (isIncreasing(allNumbers)) {
+    safeCount++;
   }
-
-  let isSafe = true;
-  let currNum;
-  let difference;
-
-  for (let i = 1; i < allNumbers.length; ++i) {
-    currNum = parseInt(allNumbers[i]);
-    difference = Math.abs(currNum - prevNum);
-
-    if (difference > 3) isSafe = false;
-    else if (isDecreasing && currNum >= prevNum) isSafe = false;
-    else if (!isDecreasing && currNum <= prevNum) isSafe = false;
-
-    prevNum = currNum;
-  }
-
-  if (isSafe) safeCount++;
 });
 
 console.log("Safe Reports:", safeCount);
+
+function isDecreasing(list) {
+  let [curr, prev, diff] = [0, 0, 0];
+
+  for (let i = 1; i < list.length; ++i) {
+    curr = parseInt(list[i]);
+    prev = parseInt(list[i - 1]);
+    diff = Math.abs(curr - prev);
+
+    if (curr >= prev || diff > 3) return false;
+  }
+
+  return true;
+}
+
+function isIncreasing(list) {
+  let [curr, prev, diff] = [0, 0, 0];
+
+  for (let i = 1; i < list.length; ++i) {
+    curr = parseInt(list[i]);
+    prev = parseInt(list[i - 1]);
+    diff = Math.abs(curr - prev);
+
+    if (curr <= prev || diff > 3) return false;
+  }
+
+  return true;
+}
